50a0a67ca1235114948ca0cbd7f4267ca8ad2eac
echo3582 <qianlu3582@163.com>
2019-03-15 13:56:13 +0800

    【功能】 触发touchstart事件则转盘旋转60°

--- turntable.js
+++ turntable.js
@@ -1,25 +1,32 @@
-function draw(){
-  let ctx = document.getElementById('canvas').getContext('2d');
-  ctx.translate(150,150);
-  ctx.scale(0.4,0.4);
-  ctx.lineWidth = 5;
+let angle = 0;
+let ctx = document.getElementById('canvas').getContext('2d');
+function draw(angle){
+  // --- 清空页面元素，用于逐帧动画
+  ctx.clearRect(0,0,300,300);
 
+  ctx.translate(150,150);
+  ctx.lineWidth = 5;
+  //第一次调用draw时angle参数为undefined，所以需要给它赋个初始值。
+  if (angle!==undefined) {
+    angleInside = angle;
+  } else {
+    angleInside = 0;
+  }
   // 长刻度
   for (var i=0;i<12;i++){
     ctx.beginPath();
     ctx.rotate(Math.PI/6);
-    ctx.moveTo(200,0);
-    ctx.lineTo(240,0);
+    ctx.moveTo(100*Math.cos(angleInside),100*Math.sin(angleInside));
+    ctx.lineTo(120*Math.cos(angleInside),120*Math.sin(angleInside));
     ctx.stroke();
   }
-
   // 短刻度
-  ctx.lineWidth = 5;
+  ctx.lineWidth = 3;
   for (i=0;i<120;i++){
     if (i%10!=0) {
       ctx.beginPath();
-      ctx.moveTo(234,0);
-      ctx.lineTo(240,0);
+      ctx.moveTo(117*Math.cos(angleInside),117*Math.sin(angleInside));
+      ctx.lineTo(120*Math.cos(angleInside),120*Math.sin(angleInside));
       ctx.stroke();
     }
     ctx.rotate(Math.PI/60);
@@ -27,8 +34,10 @@ function draw(){
 
   // 画可识别图案
   ctx.beginPath();
-  ctx.arc(0,-60,10,0,Math.PI*2,true); // 绘制
+  ctx.arc(60*Math.sin(angleInside),-60*Math.cos(angleInside),10,0,Math.PI*2,true); // 绘制
   ctx.stroke();
+  //恢复canvas原点
+  ctx.translate(-150,-150);
 }
 
 function startup() {
@@ -38,8 +47,9 @@ function startup() {
   turntable.addEventListener("touchmove", handleMove, false);
 }
 
-function handleStart() {
+function handleStart(event) {
   console.log("I'm touched");
+  draw(Math.PI/3);
 }
 
 function handleEnd() {
