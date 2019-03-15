4587823a3825bbfcac62518f79bbec5d05e21a33
echo3582 <qianlu3582@163.com>
2019-03-15 17:07:45 +0800

    【功能】 实现转盘跟随手指转动功能

--- turntable.js
+++ turntable.js
@@ -43,21 +43,34 @@ function draw(angle){
 function startup() {
   let turntable = document.getElementById("canvas");
   turntable.addEventListener("touchstart", handleStart, false);
-  turntable.addEventListener("touchend", handleEnd, false);
   turntable.addEventListener("touchmove", handleMove, false);
 }
 
+let touchPoints = [];
 function handleStart(event) {
-  console.log("I'm touched");
-  draw(Math.PI/3);
+  touchPoints.push(event.changedTouches[0].pageX);
+  touchPoints.push(event.changedTouches[0].pageY);
+  let angleTest = calculateAngle([0,0], [1,0], [0,1]);
 }
 
-function handleEnd() {
-  console.log("I'm released");
+let angleTurned = 0;
+function handleMove(event) {
+  event.preventDefault();
+  angleTurned += calculateAngle([150, 150], touchPoints.slice(-2), [event.changedTouches[0].pageX, event.changedTouches[0].pageY]);
+  draw(angleTurned);
+  touchPoints = [];
+  touchPoints.push(event.changedTouches[0].pageX);
+  touchPoints.push(event.changedTouches[0].pageY);
 }
 
-function handleMove() {
-  console.log("I'm moving")
+function calculateAngle(pointA, pointB, pointC) {
+  let lengthAB = Math.sqrt(Math.pow(pointA[0]-pointB[0], 2) + Math.pow(pointA[1]-pointB[1], 2));
+  let lengthAC = Math.sqrt(Math.pow(pointA[0]-pointC[0], 2) + Math.pow(pointA[1]-pointC[1], 2));
+  let lengthBC = Math.sqrt(Math.pow(pointB[0]-pointC[0], 2) + Math.pow(pointB[1]-pointC[1], 2));
+  let cosA = (Math.pow(lengthAB, 2) + Math.pow(lengthAC, 2) - Math.pow(lengthBC, 2)) / (2 * lengthAB * lengthAC);
+
+  let angleA = Math.acos(cosA);
+  return angleA;
 }
 
 startup();
