3f826b30d0abc5cbe1e1c5ccab70c04d4392fdba
echo3582 <qianlu3582@163.com>
2019-03-15 10:25:12 +0800

    【界面】 修改转盘大小和刻度个数（刻度改为每小格10个）

--- turntable.js
+++ turntable.js
@@ -1,40 +1,33 @@
 function draw(){
   let ctx = document.getElementById('canvas').getContext('2d');
-  ctx.save();
-  ctx.clearRect(0,0,300,300);
-  ctx.translate(75,75);
-  ctx.scale(0.5,0.5);
-  ctx.rotate(-Math.PI/2);
+  ctx.translate(150,150);
+  ctx.scale(0.4,0.4);
   ctx.lineWidth = 5;
 
   // 长刻度
-  ctx.save();
   for (var i=0;i<12;i++){
     ctx.beginPath();
     ctx.rotate(Math.PI/6);
-    ctx.moveTo(100,0);
-    ctx.lineTo(120,0);
+    ctx.moveTo(200,0);
+    ctx.lineTo(240,0);
     ctx.stroke();
   }
-  ctx.restore();
 
   // 短刻度
-  ctx.save();
   ctx.lineWidth = 5;
-  for (i=0;i<60;i++){
-    if (i%5!=0) {
+  for (i=0;i<120;i++){
+    if (i%10!=0) {
       ctx.beginPath();
-      ctx.moveTo(117,0);
-      ctx.lineTo(120,0);
+      ctx.moveTo(234,0);
+      ctx.lineTo(240,0);
       ctx.stroke();
     }
-    ctx.rotate(Math.PI/30);
+    ctx.rotate(Math.PI/60);
   }
-  ctx.restore();
 
   // 画可识别图案
   ctx.beginPath();
-  ctx.arc(30,30,10,0,Math.PI*2,true); // 绘制
+  ctx.arc(0,-60,10,0,Math.PI*2,true); // 绘制
   ctx.stroke();
 }
 
